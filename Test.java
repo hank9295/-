7f0c1dd1a24281f6291e5ca0b61c1f3e8e5d3c52
hank9295 <114204224+hank9295@users.noreply.github.com>
2022-11-25 09:39:21 +0800

    空白稜形

--- Test.java
+++ Test.java
@@ -1,19 +1,19 @@
 public class Test{	
     public static void main(String[] args){
 		
-		for(int i=0;i<9;i++){
-			for(int y=0;y<=10;y++){
-				if(y-i>5){
+		for(int i=-4;i<=4;i++){
+			for(int y=-5;y<=5;y++){
+				if(i+y<=-5||i-y>=5)
 					System.out.printf("*"); 			
-	
-				}
-				else if(i-y>3) {
-				System.out.printf("*"); 
-
-				}		
-
+				else if(i+y>4||i-y<-4)
+					System.out.printf("*");	
+				else
+					System.out.printf(" ");
+					
 			}	
         System.out.println(); 			
+		
+
 		}	
 	}
 }
